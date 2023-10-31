7ed729989f7dc528a15fa4c97c8b12c03da84ca5
Alana Selli <alana.selli@usp.br>
2023-10-31 09:29:34 -0300

    added GBLUP to simulation

--- scripts/main.R
+++ scripts/main.R
@@ -40,19 +40,19 @@ year = 0
 rec_data("01_genotypes/pedigree.txt", founderPop, 
          "Founder", year, append = FALSE)
 
-cat("\nWriting PLINK file for founder_sample_100\n")
+cli_alert_info("\nWriting PLINK file for founder_sample_100\n")
 founder_sample_100 = selectInd(founderPop, nInd = 100, use = "rand")
 writePlink(founder_sample_100, "01_genotypes/founder_sample_100")
 
-cat("\nWriting founder_phased\n")
+cli_alert_info("\nWriting founder_phased\n")
 founder_phased = getPhasedHaplo(founder_sample_100)
 write.table(founder_phased, "01_genotypes/founder_phased.txt", 
             quote = F, col.names = F)
 
-cat("\nWriting founder QTLs\n")
+cli_alert_info("\nWriting founder QTLs\n")
 writePlink(founder_sample_100, "01_genotypes/founder_QTL", useQtl = TRUE)
 
-rm(founderGenomes,founder_sample_100,founder_sample_10,founder_phased)
+rm(founderGenomes,founder_sample_100,founder_phased)
 
 # ---- Expand for 100 generations ----
 
@@ -70,9 +70,9 @@ year = year + 1
 rec_data("01_genotypes/pedigree.txt", expandedPop, 
          "Expanded", year, append = TRUE)
 
-cli_progress_bar("Expanding population", total = 10)
-for (gen in 1:100) {
-    nCrosses = round(nCrosses*1.5)
+cli_progress_bar("Expanding population", total = 50)
+for (gen in 1:50) {
+    nCrosses = round(nCrosses*1.05)
     expandedPop = randCross(
         pop = expandedPop,
         nCrosses = nCrosses,
@@ -87,19 +87,19 @@ for (gen in 1:100) {
 cli_alert_info(paste0("\n",expandedPop@nInd, 
                       " individuals in the last expansion generation.\n"))
 
-cat("\nWriting PLINK file for expanded_sample_100\n")
+cli_alert_info("\nWriting PLINK file for expanded_sample_100\n")
 expanded_sample_100 = selectInd(expandedPop, nInd = 100, use = "rand")
 writePlink(expanded_sample_100, "01_genotypes/expanded_sample_100")
 
-cat("\nWriting expanded_phased\n")
+cli_alert_info("\nWriting expanded_phased\n")
 expanded_phased = getPhasedHaplo(expanded_sample_100)
 write.table(expanded_phased, "01_genotypes/expanded_phased.txt", 
             quote = F, col.names = F)
 
-cat("\nWriting expanded QTLs\n")
+cli_alert_info("\nWriting expanded QTLs\n")
 writePlink(expanded_sample_100, "01_genotypes/expanded_QTL", useQtl = TRUE)
 
-rm(expanded_sample_100, expanded_sample_10, expanded_phased)
+rm(expanded_sample_100, expanded_phased)
 
 # ---- Recent Population ----
 
@@ -117,20 +117,33 @@ recentPop = makeRecentPop(previous_pop = expandedPop,
                                            TRUE))
 recentPop = recentPop[[1]]
 
-cat("\nWriting PLINK file for recent\n")
+# Fit RR-BLUP model for genomic predictions
+ans = RRBLUP(recentPop, simParam=SP)
+recentPop = setEBV(recentPop, ans, simParam=SP)
+
+cli_alert_info("\nCalculating and writing EBVs\n")
+BLUP = data.frame(ID = pop@id, 
+                  EBV = pop@ebv, 
+                  GV = pop@gv)
+names(BLUP) = c('ID','EBV','GV')
+
+write.table(BLUP,"BLUP_AlphaSimR.txt", row.names = F, quote = F)
+
+cli_alert_info("\nWriting PLINK file for recent\n")
 writePlink(recentPop, "01_genotypes/recent")
 
-cat("\nWriting PLINK file for recent_sample_100\n")
+cli_alert_info("\nWriting PLINK file for recent_sample_100\n")
 recentPop_sample_100 = selectInd(recentPop, nInd = 100, use = "rand")
 writePlink(recentPop_sample_100, "01_genotypes/recent_sample_100")
 
-cat("\nWriting recent_phased\n")
-recentPop_sample_10 = selectInd(recentPop, nInd = 10, use = "rand")
+cli_alert_info("\nWriting recent_phased\n")
 recentPop_phased = getPhasedHaplo(recentPop_sample_100)
 write.table(recentPop_phased, "01_genotypes/recent_phased.txt", 
             quote = F, col.names = F)
 
-cat("\nWriting Pop A QTLs\n")
+cli_alert_info("\nWriting Pop A QTLs\n")
 writePlink(recentPop_sample_100, "01_genotypes/recent_QTL", useQtl = TRUE)
 
-rm(recentPop_sample_100, recentPop_sample_10, recentPop_phased)
+rm(recentPop_sample_100, recentPop_phased)
+
+cli_alert_success("\nSimulation completed.\n")
